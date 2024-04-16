1036c3e9ccda49ec6ff080d241d230f52d85c24e
Karthick Ganesan <karthick.ganesh@outlook.com>
2024-04-16 09:32:08 +0530

    Accessing Query String Parameter

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import { sort } from "fast-sort";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface Props {
+  sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+  const sortUsers = sort(users).asc(
+    sortOrder === "email"
+      ? (user) => user.email
+      : sortOrder === "name"
+      ? (user) => user.name
+      : (user) => user.id
+  );
+
+  return (
+    <table className="table table-zebra">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>
+            <Link href="/users?sortOrder=name">Name</Link>
+          </th>
+          <th>
+            <Link href="/users?sortOrder=email">Email</Link>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortUsers.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
